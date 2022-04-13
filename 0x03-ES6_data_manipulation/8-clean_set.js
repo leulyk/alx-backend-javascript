582980a9d727a4d2477e9b802f512638d5afb5c0
Leul Yiheyis <leulykpro@gmail.com>
2022-04-13 20:45:28 +0300

    add type checking of the start string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const arr = [];
-  if (startString) {
+  if (startString && typeof startString === 'string') {
     for (const item of set) {
       if (item.startsWith(startString) && item !== startString) {
         arr.push(item.substr(startString.length));
