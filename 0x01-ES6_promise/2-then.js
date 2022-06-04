c88dd68201da26476659487e9e2c53fac9e9bfd4
Leul Yiheyis <leulykpro@gmail.com>
2022-06-04 18:07:31 +0300

    Use console.log instead, and fix typo

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'Success ' }))
+    .then(() => ({ status: 200, body: 'success ' }))
     .catch(() => new Error())
-    .finally(() => process.stdout.write('Got a response from the API\n'));
+    .finally(() => console.log('Got a response from the API\n'));
 }
