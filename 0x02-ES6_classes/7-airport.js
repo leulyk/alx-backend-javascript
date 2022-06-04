0489ac8cde24cb6ec0c1aa4617a1fae8295cce71
Leul Yiheyis <leulykpro@gmail.com>
2022-06-04 11:10:08 +0300

    new class 'Airport' demonstrating default string description

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  /*
+  toString() {
+    return this._code;
+  }
+  */
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
