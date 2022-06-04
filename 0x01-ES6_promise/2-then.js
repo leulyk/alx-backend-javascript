df31fd8f9073238f113603dd1e2466961c8cd1cd
Leul Yiheyis <leulykpro@gmail.com>
2022-06-04 18:04:21 +0300

    add a finally block, and fix the then block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      process.stdout.write('Got a response from the API\n');
-      return { status: 200, body: 'Success ' };
-    })
-    .catch(new Error());
+    .then(() => ({ status: 200, body: 'Success ' }))
+    .catch(() => new Error())
+    .finally(() => process.stdout.write('Got a response from the API\n'));
 }
