8fa3a2f8d7158d9efa06db3842e6e291ea2aafec
Leul Yiheyis <leulykpro@gmail.com>
2022-06-09 20:33:22 +0300

    simple HTTP server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+/*
+ * First web server
+ */
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = '1245';
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+
+module.exports = app;
